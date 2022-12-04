72a7a35085f61d3756c8a8f4a7da2f2d687d42f8
krazyunderground <charlieprescod@outlook.com>
2022-12-04 23:43:05 +0000

    +assets +voxnet-v2 %website-title

--- src/root.tsx
+++ src/root.tsx
@@ -19,7 +19,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With MDX</Title>
+        <Title>KrazyUG's page</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
@@ -34,7 +34,6 @@ export default function Root() {
               </main>
             </Suspense>
           </ErrorBoundary>
-          
         <Scripts />
       </Body>
     </Html>
