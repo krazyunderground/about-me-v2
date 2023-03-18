1af127c16d436cf9c30193c9f167b0d9d33244fd
krazyunderground <charlieprescod@outlook.com>
2023-03-18 20:09:00 +0000

    game is now centred on page

--- src/components/BiggestBirdGame.tsx
+++ src/components/BiggestBirdGame.tsx
@@ -1,7 +1,7 @@
 const BBG = () => {
   return (
     <div>
-        <iframe mozallowfullscreen="true" allow="autoplay; fullscreen" src="./biggestbirdgame/index.html" style="border:0px #000000 none;" name="Biggest Bird Game" scrolling="no" msallowfullscreen="true" allowfullscreen="true" webkitallowfullscreen="true" allowtransparency="true" frameborder="0" marginheight="px" marginwidth="320px" height="540px" width="960px"></iframe>
+        <iframe mozallowfullscreen="true" allow="autoplay; fullscreen" src="./biggestbirdgame/index.html" style="border:0px #000000 none;" name="Biggest Bird Game" scrolling="no" msallowfullscreen="true" allowfullscreen="true" webkitallowfullscreen="true" allowtransparency="true" frameborder="0" marginheight="px" height="540px" width="960px"></iframe>
     </div>
   )
 }
