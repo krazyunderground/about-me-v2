afb4017bb06a13e59b61a87c57c8c6e140c7ff1f
krazyunderground <krazy@krazyunderground.xyz>
2026-04-23 14:18:01 +0100

    Make PetalPatch iframe responsive: scale to container width

--- src/components/PetalPatch.tsx
+++ src/components/PetalPatch.tsx
@@ -1,12 +1,36 @@
+import { createSignal, onMount, onCleanup } from "solid-js";
+
+const GAME_W = 1280;
+const GAME_H = 720;
+
 const PP = () => {
+  let containerRef: HTMLDivElement | undefined;
+  const [scale, setScale] = createSignal(1);
+
+  const updateScale = () => {
+    if (containerRef) {
+      const s = containerRef.clientWidth / GAME_W;
+      setScale(s);
+    }
+  };
+
+  onMount(() => {
+    updateScale();
+    window.addEventListener("resize", updateScale);
+  });
+
+  onCleanup(() => {
+    window.removeEventListener("resize", updateScale);
+  });
+
   return (
-    <div style="display:flex; justify-content:center;">
-      <div style="width:960px; height:540px; overflow:hidden; flex-shrink:0;">
-          {/* @ts-expect-error vendor attributes (mozallowfullscreen, msallowfullscreen, webkitallowfullscreen) not in JSX types */}
-          <iframe mozallowfullscreen={true} allow="autoplay; fullscreen" src="./petalpatch/petalpatch.html" style="border:0px #000000 none; transform:scale(0.75); transform-origin:top left;" name="Petal Patch" scrolling="no" msallowfullscreen={true} allowfullscreen={true} allowtransparency={true} frameborder="0" height="720px" width="1280px"></iframe>
+    <div ref={containerRef} style={`width:100%; max-width:${GAME_W}px; margin:0 auto; height:${GAME_H * scale()}px; overflow:hidden;`}>
+      <div style={`width:${GAME_W}px; height:${GAME_H}px; transform:scale(${scale()}); transform-origin:top left;`}>
+        {/* @ts-expect-error vendor attributes (mozallowfullscreen, msallowfullscreen, webkitallowfullscreen) not in JSX types */}
+        <iframe mozallowfullscreen={true} allow="autoplay; fullscreen" src="./petalpatch/petalpatch.html" style="border:0 none;" name="Petal Patch" scrolling="no" msallowfullscreen={true} allowfullscreen={true} allowtransparency={true} frameborder="0" height={`${GAME_H}px`} width={`${GAME_W}px`}></iframe>
       </div>
     </div>
-  )
+  );
 }
 
 export default PP
